99b4595da151eb9b3428303291160d042c0be549
Ricardo Tassio Dantas <ricardotassio@nunchisolucoes.com>
2024-05-03 17:55:47 -0400

    chore: add huscky and lint-staged in package.json

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,12 @@
 const config = {
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
-  roots: ["<rootDir>/src"],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  roots: ['<rootDir>/src'],
   transform: {
-    "^.+\\.ts$": "ts-jest"
-  },
-};
+    '^.+\\.ts$': 'ts-jest'
+  }
+}
 
-module.exports = config;
+module.exports = config
---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,15 @@
+import { SignUpController } from './signup';
+describe('Signup Controller', () => {
+    test('Should return 400 if no name is provided', () => {
+        const sut = new SignUpController();
+        const httpRequest = {
+            body: { 
+                email: 'user@mail.com',
+                password: 'password',
+                passwordConfirmation: 'password'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+    })
+}) 
