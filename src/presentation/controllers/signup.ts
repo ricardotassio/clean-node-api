b926a01027b57f5bfb2fee62d0a3eb3a8575439c
Ricardo Tassio Dantas <ricardotassio@nunchisolucoes.com>
2024-05-04 16:07:20 -0400

    refactor: remove duplication code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -15,14 +15,14 @@ export class SignUpController implements Controller {
         return badRequest(new MissingParamError('body'))
       }
 
-      const { email } = httpRequest.body
+      const { email, password, passwordConfirmation } = httpRequest.body
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field))
         }
       }
-      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
       const isValid = this.emailValidator.isValid(email)
