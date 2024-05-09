f93bf275bbe5a1e3c7189c90caa5803218234d3c
Ricardo Tassio Dantas <ricardotassio@nunchisolucoes.com>
2024-05-09 11:03:40 -0400

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -10,7 +10,10 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.disconnect()
   })
 
-  
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
   const makeSut = (): AccountMongoRepository => {
     return new AccountMongoRepository()
   }
